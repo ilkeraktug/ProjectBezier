54991ae09f137198651b73086366fdff4bcfbb66
İlker Aktuğ <63074357+ilkeraktug@users.noreply.github.com>
2025-06-15 01:50:19 +0300

    Prevent moving control points up direction in order to fix some bugs

--- Assets/Scripts/SplineComponent/SplineComponentEditor.cs
+++ Assets/Scripts/SplineComponent/SplineComponentEditor.cs
@@ -13,7 +13,10 @@ public class SplineComponentEditor : Editor
             for (int i = 0; i < asSplineComponent.BezierCurveControlPoints.Count; ++i)
             {
                 Vector3 NewPointPosition = Handles.PositionHandle(asSplineComponent.BezierCurveControlPoints[i], asSplineComponent.transform.rotation);
-
+                
+                // 2 spline'nın kesiştiği durumları sadece XZ plane'i üzerinde test ediyorum. O nedenle splineların yükseklik kısmını kapattım.
+                NewPointPosition.y = asSplineComponent.transform.position.y;
+                
                 if (asSplineComponent.BezierCurveControlPoints[i] != NewPointPosition)
                 {
                     asSplineComponent.BezierCurveControlPoints[i] = NewPointPosition;
